2af1b44a654f72c8f8307d5895cdc1f890af2011
ducanh2k3 <105410311+ducanh2k3@users.noreply.github.com>
2023-05-22 14:28:04 +0700

    update main

--- src/app/common/main.ts
+++ src/app/common/main.ts
@@ -4,4 +4,5 @@ export interface Products{
     price: number;
     description: string;
     imgUrl: string;
+    type:string;
 }
--- src/app/data/main.ts
+++ src/app/data/main.ts
@@ -1,51 +1,52 @@
-import { Products } from "../common/main"
+import { Products } from '../common/main';
 
 export const producttt: Products[] = [
-   { id: '1',
+  {
+    id: '1',
     name: 'linh tinh',
     price: 200,
     description: '10%',
-    imgUrl:
-      'https://picsum.photos/200/200',
+    imgUrl: 'https://picsum.photos/200/200',
+    type: 'women',
   },
   {
     id: '2',
     name: 'fsasa',
     price: 300,
     description: '20%',
-    imgUrl:
-      'https://picsum.photos/200/200',
+    imgUrl: 'https://picsum.photos/200/200',
+    type: 'men',
   },
   {
     id: '3',
     name: 'dsad',
     price: 400,
     description: '30%',
-    imgUrl:
-      'https://picsum.photos/200/200',
+    imgUrl: 'https://picsum.photos/200/200',
+    type: 'men',
   },
   {
     id: '3',
     name: 'dsad',
     price: 400,
     description: '30%',
-    imgUrl:
-      'https://picsum.photos/200/200',
+    imgUrl: 'https://picsum.photos/200/200',
+    type: 'women',
   },
   {
     id: '3',
     name: 'dsad',
     price: 400,
     description: '30%',
-    imgUrl:
-      'https://picsum.photos/200/200',
+    imgUrl: 'https://picsum.photos/200/200',
+    type: 'women',
   },
   {
     id: '3',
     name: 'dsad',
     price: 400,
     description: '30%',
-    imgUrl:
-      'https://picsum.photos/200/200',
+    imgUrl: 'https://picsum.photos/200/200',
+    type: 'women',
   },
-]
+];
