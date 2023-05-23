0bd322623730de6977d65eb53f79eea9a7ad8f54
ducanh2k3 <105410311+ducanh2k3@users.noreply.github.com>
2023-05-23 08:40:09 +0700

    detail product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
@@ -35,8 +35,7 @@ import { MainComponent } from './component/main/main.component';
     ProducttComponent,
 
     NavbarComponent,
-    MainComponent
-
+    MainComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
--- src/app/component/product-detail/product-detail.component.ts
+++ src/app/component/product-detail/product-detail.component.ts
@@ -1,11 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Products } from 'src/app/common/main';
-
+import { ActivatedRoute } from '@angular/router';
+import { producttt } from 'src/app/data/main';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent {
-  @Input('data') product!: Products;
+  constructor(private route: ActivatedRoute) {}
+  product: Products | undefined;
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      console.log(id);
+
+      this.product = producttt.find((item) => item.id === id);
+console.log(this.product);
+      // Fetch product details based on the ID and update the component's properties
+    });
+  }
 }
